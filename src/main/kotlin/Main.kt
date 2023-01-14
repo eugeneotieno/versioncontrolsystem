33050d53337a4b9b0d316039e5c0ad28c9a834b8
Eugene Otieno <otienoeo@gmail.com>
2023-01-14 11:05:45 +0300

    Finished implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
 
 const val COMMANDS = """These are SVCS commands:
 config     Get and set a username.
@@ -20,6 +22,12 @@ fun main() {
 
     val indexFile = vcsDir.resolve("index.txt")
     if (!indexFile.exists()) indexFile.createNewFile()
+
+    val logFile = vcsDir.resolve("log.txt")
+    if (!logFile.exists()) logFile.createNewFile()
+
+    val commitsDir = File("${workingDirectory}${separator}vcs${separator}commits")
+    if (!commitsDir.exists()) commitsDir.mkdir()
 
     print("Enter command or --help for commands: ")
     val cmdInput = readln().split(" ")
@@ -64,9 +72,81 @@ fun main() {
                 }
             }
         }
-        "log" -> println("Show commit logs.")
-        "commit" -> println("Save changes.")
-        "checkout" -> println("Restore a file.")
+        "log" -> {
+            if (logFile.readText().isBlank()) {
+                println("No commits yet.")
+            } else {
+                println(logFile.readText())
+            }
+        }
+        "commit" -> {
+            if (cmdInput.size > 1) {
+
+                var content = ""
+                val trackedFiles = vcsDir.parentFile.listFiles()
+                for (file in trackedFiles!!) {
+                    if (file.name != "untracked_file.txt") {
+                        if (!file.isDirectory) {
+                            content += file.readText()
+                        }
+                    }
+                }
+
+                val md = MessageDigest.getInstance("SHA-256")
+                val messageDigest = md.digest(content.toByteArray())
+                val no = BigInteger(1, messageDigest)
+                var hashtext = no.toString(16)
+                while (hashtext.length < 32) {
+                    hashtext = "0$hashtext"
+                }
+
+                val hashDir = File("${workingDirectory}${separator}vcs${separator}commits${separator}${hashtext}")
+                if (hashDir.exists()) {
+                    println("Nothing to commit.")
+                } else {
+                    hashDir.mkdir()
+
+                    for (file in trackedFiles) {
+                        if (file.name != "untracked_file.txt") {
+                            if (!file.isDirectory) {
+                                val newFile = hashDir.resolve(file.name)
+                                if (!newFile.exists()) newFile.createNewFile()
+                                newFile.writeText(file.readText())
+                            }
+                        }
+                    }
+
+                    val logMsg = "commit $hashtext\nAuthor: ${configFile.readText().split(" ").last().replace(".", "")}\n${cmdInput[1]}"
+                    if (logFile.readText().isBlank()) {
+                        logFile.writeText(logMsg)
+                    } else {
+                        val prevCommits = logFile.readText()
+                        logFile.writeText("$logMsg\n\n$prevCommits")
+                    }
+
+                    println("Changes are committed.")
+                }
+            } else {
+                println("Message was not passed.")
+            }
+        }
+        "checkout" -> {
+            if (cmdInput.size > 1) {
+                val commitDir = File("${workingDirectory}${separator}vcs${separator}commits${separator}${cmdInput[1]}")
+                if (commitDir.exists()) {
+                    for (file in commitDir.listFiles()!!) {
+                        val newFile = vcsDir.parentFile.resolve(file.name)
+                        if (!newFile.exists()) newFile.createNewFile()
+                        newFile.writeText(file.readText())
+                    }
+                    println("Switched to commit ${cmdInput[1]}.")
+                } else {
+                    println("Commit does not exist.")
+                }
+            } else {
+                println("Commit id was not passed.")
+            }
+        }
         else -> println("'${cmdInput[0]}' is not a SVCS command.")
     }
 }
